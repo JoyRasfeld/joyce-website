cd1495fb3111175f6830217232bbce7e485344ad
Adam Rasfeld <arasfeld@gmail.com>
2025-09-11 14:12:16 -0400

    make contact page content better centered

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,8 +21,8 @@ export default function Contact() {
 
       {/* Contact Form and Info */}
       <section className="py-20">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex gap-12">
             {/* Contact Form */}
             <ContactForm />
 
