42bb3884cc935a9a75f64ce3a85bc41c741aab15
Adam Rasfeld <arasfeld@gmail.com>
2025-09-11 21:57:57 -0400

    fix contact page on mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -22,7 +22,7 @@ export default function Contact() {
       {/* Contact Form and Info */}
       <section className="py-20">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex gap-12 justify-center">
+          <div className="flex flex-col lg:flex-row gap-12 justify-center">
             {/* Contact Form */}
             <ContactForm />
 
